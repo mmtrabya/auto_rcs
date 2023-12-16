8a1d49d18c13246893423bbfbaeeb62bb0e2dac2
Mohammed Tarabay <midotarabay66@gmail.com>
2023-12-16 02:30:27 +0200

    Lidar Update

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -74,9 +74,9 @@ def generate_launch_description():
             output='screen'),
 
         #Node(
-            #package='rviz2',
-            #executable='rviz2',
-           # name='rviz2',
-           # arguments=['-d', rviz_config_dir],
-           # output='screen'),
+           #package='rviz2',
+           #executable='rviz2',
+           #name='rviz2',
+           #arguments=['-d', rviz_config_dir],
+           #output='screen'),
     ])
